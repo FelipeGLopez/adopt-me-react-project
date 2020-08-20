1e3a5a8b7de77085053b6388393c21ad031fd09d
Felipe <fglf18@gmail.com>
2020-08-19 22:36:36 -0300

    Code splitting section

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, lazy, Suspense } from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Suspense fallback={<h1>loading route …</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
